2076eacfa4980d2b4cdf38fd50431e503c2f141c
nijat ibrahimov <nijatibrahimov@gmail.com>
2022-11-16 19:18:13 +0400

    add: lualine.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,16 +1,18 @@
+
 local status, packer = pcall(require, 'packer')
 if (not status) then
   print("Packer is not installed")
   return
 end
 
-
 vim.cmd [[packadd packer.nvim ]]
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  use {
-    'svrana/neosolarized.nvim',
+  use { 'svrana/neosolarized.nvim',
     requires = { 'tjdevries/colorbuddy.nvim' }
-}
+  }
+  use { 'hoob3rt/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 end)
