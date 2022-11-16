a8daf5184f598e0e50ba1736bb4bbc8adca25888
nijat ibrahimov <nijatibrahimov@gmail.com>
2022-11-16 18:03:53 +0400

    add: neosolarized.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -22,8 +22,26 @@ keymap.set('n', '<S-Tab>', ':tabprevious<CR>')
 keymap.set('n', 'te', ':tabedit<CR>', { silent = true })
 
 -- windows split
-keymap.set('n', 'ss', ':split<CR><C-w>w')
-keymap.set('n', 'sv', ':vsplit<CR><C-w>w')
+keymap.set('n', 'ss', ':split<CR><C-w>w', { silent = true })
+keymap.set('n', 'sv', ':vsplit<CR><C-w>w', { silent = true })
+
+-- windows navigation
+keymap.set('n', '<Space>', '<C-w>w')
+keymap.set('', 's<left>', '<C-w>h')
+keymap.set('', 's<right>', '<C-w>l')
+keymap.set('', 's<up>', '<C-w>k')
+keymap.set('', 's<down>', '<C-w>j')
+keymap.set('', 'sh', '<C-w>h')
+keymap.set('', 'sl', '<C-w>l')
+keymap.set('', 'sk', '<C-w>k')
+keymap.set('', 'sj', '<C-w>j')
+
+
+-- windows resize
+keymap.set('n', '<C-w><left>', '<C-w><')
+keymap.set('n', '<C-w><right>', '<C-w>>')
+keymap.set('n', '<C-w><up>', '<C-w>+')
+keymap.set('n', '<C-w><down>', '<C-w>-')
 
 -- line coordination
 keymap.set('n', '<Space>h', '0')
