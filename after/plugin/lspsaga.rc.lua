b49b9a8c49b0bd9a25c2b77f614d4a8e0758f95e
nijat ibrahimov <ibrahimovofficial@gmail.com>
2023-02-17 00:56:27 +0400

    update: fix sumneko_lua deprecated error. update mason.

--- after/plugin/lspsaga.rc.lua
+++ after/plugin/lspsaga.rc.lua
@@ -1,7 +1,7 @@
 local saga_status, saga = pcall(require, 'lspsaga')
 if (not saga_status) then return end
 
-saga.init_lsp_saga({
+saga.setup({
   move_in_saga = {
     prev = '<C-k>',
     next = '<C-j>'
--- after/plugin/mason.rc.lua
+++ after/plugin/mason.rc.lua
@@ -11,6 +11,6 @@ mason_lspconfig.setup({
     "html",
     "cssls",
     "tailwindcss",
-    "sumneko_lua",
+    "lua_ls"
   }
 })
