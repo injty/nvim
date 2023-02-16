bc8e4576764fd1b16f4d1d74d66ed513295eb050
nijat ibrahimov <ibrahimovofficial@gmail.com>
2023-02-16 11:50:43 +0400

    update: neosolarized + colorbuddy.

--- after/plugin/neosolarized.rc.lua
+++ after/plugin/neosolarized.rc.lua
@@ -3,5 +3,18 @@ if (not status) then return end
 
 n.setup({
   comment_italics = false,
-  background_set = true,
+  background_set = false,
 })
+
+local cb = require('colorbuddy.init')
+local Color = cb.Color
+local colors = cb.colors
+local Group = cb.Group
+local styles = cb.styles
+
+Color.new('white', '#ffffff')
+Color.new('black', '#000000')
+Group.new('Normal', colors.base1, colors.NONE, styles.NONE)
+Group.new('CursorLine', colors.none, colors.base03, styles.NONE, colors.base1)
+Group.new('CursorLineNr', colors.yellow, colors.base03, styles.NONE, colors.base1)
+Group.new('Visual', colors.none, colors.base03, styles.reverse)
