121bd53c75a06d119248b98eca1995188ea864d8
nijatibrahimov <51142227+nijatibrahimov@users.noreply.github.com>
2023-10-23 01:45:07 +0400

    ⚡update: theme.

--- after/plugin/one-monokai.rc.lua
+++ after/plugin/one-monokai.rc.lua
@@ -1,4 +1,4 @@
-local status, theme = pcall(require, "one_monokai")
-if (not status) then return end
-
-theme.setup({})
+-- local status, theme = pcall(require, "one_monokai")
+-- if (not status) then return end
+--
+-- theme.setup({})
