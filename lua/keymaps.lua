64ed00901ac92251aa2f1f1ad6d2ceed03f14e76
injty <ibrahimovofficial@gmail.com>
2025-02-09 20:30:22 +0400

    update(plugins, theme): refactoring in plugins.lua and add catppuccin theme

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,34 +1,35 @@
 local g = vim.g
 local k = vim.keymap
 
+-- Настройка leader-keys
 g.mapleader = ' '
 
--- remove ctrl-z for windows
+-- Отключение Ctrl-z для Windows
 k.set('n', '<C-z>', '<Nop>')
 
--- remove yank with x
+-- Удаление "yank" с помощью x
 k.set('n', 'x', '"_x')
 
--- increment and decriment
+-- Инкремент и декремент
 k.set('n', '+', '<C-a>')
 k.set('n', '-', '<C-x>')
 
--- delete a previous word
+-- Удалить предыдущее слово
 k.set('n', 'dw', 'vb"_d')
 
--- select all
+-- Выделение всего
 k.set('n', '<C-a>', 'gg<S-v>G')
 
--- tabs
+-- Табуляция
 k.set('n', '<Tab>', ':tabnext<CR>')
 k.set('n', '<S-Tab>', ':tabprevious<CR>')
 k.set('n', 'te', ':tabedit<CR>', { silent = true })
 
--- windows split
+-- Окна и разделение
 k.set('n', 'sg', ':split<CR><C-w>w', { silent = true })
 k.set('n', 'sv', ':vsplit<CR><C-w>w', { silent = true })
 
--- windows navigation
+-- Навигация по окнам
 k.set('n', '<Space>', '<C-w>w')
 k.set('', 's<left>', '<C-w>h')
 k.set('', 's<right>', '<C-w>l')
@@ -39,20 +40,24 @@ k.set('', 'sl', '<C-w>l')
 k.set('', 'sk', '<C-w>k')
 k.set('', 'sj', '<C-w>j')
 
+-- Изменение размера окон
+k.set('n', '<C-w><left>', ':vertical resize -2<CR>')
+k.set('n', '<C-w><right>', ':vertical resize +2<CR>')
+k.set('n', '<C-w><up>', ':resize +2<CR>')
+k.set('n', '<C-w><down>', ':resize -2<CR>')
 
--- windows resize
-k.set('n', '<C-w><left>', '<C-w><')
-k.set('n', '<C-w><right>', '<C-w>>')
-k.set('n', '<C-w><up>', '<C-w>+')
-k.set('n', '<C-w><down>', '<C-w>-')
-
--- line coordination
+-- Координация строк
 k.set('n', '<Space>h', '0')
 k.set('n', '<Space>l', '$')
 
--- telescope keymaps
+-- Telescope
 local builtin = require('telescope.builtin')
 k.set('n', '<leader>ff', builtin.find_files, {})
 k.set('n', '<leader>fg', builtin.live_grep, {})
 k.set('n', '<leader>fb', builtin.buffers, {})
 k.set('n', '<leader>fh', builtin.help_tags, {})
+k.set('n', '<leader>fd', builtin.diagnostics, {})
+k.set('n', '<leader>fo', builtin.oldfiles, {})
+
+-- Другие команды
+vim.api.nvim_set_keymap('n', '<Esc>', ':noh<CR>', { noremap = true, silent = true })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,84 +1,26 @@
 require("lazy").setup({
   {"ThePrimeagen/vim-be-good"},
   {"folke/which-key.nvim", opts = {}},
-  {"ellisonleao/gruvbox.nvim", priority = 1000 , config = true},
+
+  {"ellisonleao/gruvbox.nvim", priority = 1000, opts = {
+    terminal_colors = true,
+    contrast = "hard",
+    italic = { comments = true },
+    inverse = true
+  }},
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
   {"nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }},
-  {"nvim-telescope/telescope.nvim", tag = "0.1.6", dependencies = { 'nvim-lua/plenary.nvim' }},
-  {"craftzdog/solarized-osaka.nvim", lazy = false, priority = 1000, opts = {}}
+
+  {"nvim-telescope/telescope.nvim", tag = "0.1.6", cmd = "Telescope", dependencies = { "nvim-lua/plenary.nvim" }}
 })
 
-require("telescope").setup{
-  pickers = {
-    find_files = {
-      theme = "dropdown",
-    }
-  }
-}
+local theme = "catppuccin"
+vim.cmd("colorscheme " .. theme)
 
-require('lualine').setup {
+require("lualine").setup({
   options = {
-    icons_enabled = true,
-    theme = 'gruvbox',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
-
-require("gruvbox").setup({
-  terminal_colors = true, -- add neovim terminal colors
-  undercurl = true,
-  underline = true,
-  bold = false,
-  italic = {
-    strings = false,
-    emphasis = false,
-    comments = true,
-    operators = false,
-    folds = false,
-  },
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "hard", -- can be "hard", "soft" or empty string
-  palette_overrides = {},
-  overrides = {},
-  dim_inactive = false,
-  transparent_mode = false,
+    theme = vim.g.colors_name,
+  }
 })
 
-vim.cmd("colorscheme gruvbox")
