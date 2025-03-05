1b95f1ef4df30c067f78474fc926d685294579af
injty <ibrahimovofficial@gmail.com>
2025-03-05 11:22:16 +0400

    update: plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,26 +1,22 @@
 require("lazy").setup({
-  {"ThePrimeagen/vim-be-good"},
-  {"folke/which-key.nvim", opts = {}},
-
-  {"ellisonleao/gruvbox.nvim", priority = 1000, opts = {
-    terminal_colors = true,
-    contrast = "hard",
-    italic = { comments = true },
-    inverse = true
-  }},
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-  {"nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }},
-
-  {"nvim-telescope/telescope.nvim", tag = "0.1.6", cmd = "Telescope", dependencies = { "nvim-lua/plenary.nvim" }}
+  { "folke/which-key.nvim", opts = {} },
+  { "craftzdog/solarized-osaka.nvim", lazy = false, priority = 1000, opts = {} },
+  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }},
+  { "nvim-telescope/telescope.nvim", tag = "0.1.6", cmd = "Telescope", dependencies = { "nvim-lua/plenary.nvim" }},
+  { "neovim/nvim-lspconfig",
+    config = function()
+      require'lspconfig'.ts_ls.setup{}
+      end
+  }
 })
 
-local theme = "catppuccin"
+local theme = "solarized-osaka"
 vim.cmd("colorscheme " .. theme)
 
 require("lualine").setup({
   options = {
-    theme = vim.g.colors_name,
+    -- theme = vim.g.colors_name,
+    theme = "solarized"
   }
 })
 
